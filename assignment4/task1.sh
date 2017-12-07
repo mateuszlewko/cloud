15c6ba1fb37d8a5e73c6b9b6f8c897ffaf7f5cc0
Mateusz Lewko <mateusz.lewko@gmail.com>
2017-12-07 22:53:52 +0100

    task 4.1 WIP

--- assignment4/task1.sh
+++ assignment4/task1.sh
@@ -1,3 +1,12 @@
 sudo apt install docker.io
 
 sudo docker run -p 80:80 --name the-nginx -d nginx
+
+# haproxy dockerimage which binds port 80 (loadbalancer) to 79 (app)
+cd haproxy
+
+sudo docker build -t my-haproxy .
+
+sudo docker login
+sudo docker tag my-haproxy mateuszlewko/ii-uwr-cloud-haproxy 
+sudo docker push docker.io/mateuszlewko/ii-uwr-cloud-haproxy:latest

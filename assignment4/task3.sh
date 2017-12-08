38c762e418df8ae562463143d8924b8ea0a01811
Mateusz Lewko <mateusz.lewko@gmail.com>
2017-12-08 01:19:25 +0100

    Minor

--- assignment4/task3.sh
+++ assignment4/task3.sh
@@ -3,9 +3,9 @@ sudo docker run -p 5102:5000 -e APP_MSG="I'm a number two." --name app-sd-2 --rm
 sudo docker run -p 5103:5000 -e APP_MSG="I'm a number three." --name app-sd-3 --rm -d mateuszlewko/ii-uwr-cloud-python-app:latest
 
 # in order to update
-# build
-# push
-# pull
+# 1. build
+# 2. push
+# 3. pull
 sudo docker rm -g app-sd-1
 sudo docker rm -g app-sd-2
 sudo docker rm -g app-sd-3
